58c30ffda645517b690ed2393b7b144041ee2feb
alfathir rizky harsya <alfathirrizky083@gmail.com>
2026-01-11 15:51:17 +0700

    responsive view

--- frontend/src/pages/contentPage.jsx
+++ frontend/src/pages/contentPage.jsx
@@ -4,24 +4,24 @@ function ContentPage() {
     const { contents = [] } = useGallery();
 
     return (
-        <div className="h-auto mt-24 p-5 flex flex-wrap gap-5 justify-center">
-        {contents.map((content) => (
-            <div
-            key={content.id}
-            style={{
-                backgroundImage: `url(http://localhost:5000/uploads/${content.image})`,
-            }}
-            className="flex h-70 w-2xl items-end justify-between p-5 rounded-2xl shadow-2xl bg-cover bg-center border-4 border-white"
-            >
-            <div className="text-white drop-shadow-lg">
-                <h1 className="text-2xl font-bold">{content.caption}</h1>
-                <p>{content.description}</p>
-            </div>
-            <button className="bg-blue-500 text-white px-5 py-2 rounded-2xl font-semibold">
-                View Details
-            </button>
-            </div>
-        ))}
+        <div className="mt-24 p-5 grid grid-cols-1 md:grid-cols-2 gap-5">
+            {contents.map((content) => (
+                <div
+                key={content.id}
+                style={{
+                    backgroundImage: `url(http://localhost:5000/uploads/${content.image})`,
+                }}
+                className="flex h-70 lg:w-2xl items-end justify-between p-5 rounded-2xl shadow-2xl bg-cover bg-center border-4 border-white"
+                >
+                <div className="text-white drop-shadow-lg">
+                    <h1 className="text-2xl font-bold">{content.caption}</h1>
+                    <p>{content.description}</p>
+                </div>
+                <button className="bg-blue-500 text-white px-5 py-2 rounded-2xl font-semibold">
+                    View Details
+                </button>
+                </div>
+            ))}
         </div>
     );
 }
--- frontend/src/pages/surveiPage.jsx
+++ frontend/src/pages/surveiPage.jsx
@@ -3,16 +3,16 @@ import useSurvey from '@/hooks/useSurvey';
 export default function SurveiPage() {
     const {surveys} = useSurvey();
     return (
-        <div className="h-auto mt-24 p-5 flex gap-5 justify-center">
+        <div className="mt-13 md:mt-20 p-5 grid grid-cols-1 md:grid-cols-2 gap-5">
             {surveys.map((survey)=>(
-                <div key={survey.id} className="bg-white flex flex-col justify-center h-70 w-2xl p-5 rounded-2xl shadow-lg border-4 border-white gap-2">
-                    <img style={{backgroundImage: `url(http://localhost:5000/uploads/${survey.image})`,}} alt="" className='rounded-xl h-40 object-cover' />
+                <div key={survey.id} className="bg-white flex flex-col justify-center h-70 lg:w-2xl p-5 rounded-2xl shadow-sm border-4 border-white gap-2">
+                    <img style={{backgroundImage: `url(http://localhost:5000/uploads/${survey.image})`,}} alt="" className='rounded-xl h-40 md:h-60 bg-cover bg-center' />
                     <div className='flex justify-between items-center'>
                         <div className="text-black">
-                            <h1 className="text-2xl font-bold">{survey.title}</h1>
-                            <p className="">{survey.caption}</p>
+                            <h1 className="text-lg md:text-2xl font-bold">{survey.title}</h1>
+                            <p className="text-sm">{survey.caption}</p>
                         </div>
-                        <button className="bg-blue-500 text-white px-5 py-2 rounded-2xl font-semibold cursor-pointer">Start Survey</button>
+                        <button className="bg-blue-500 text-white px-5 py-2 rounded-2xl font-semibold cursor-pointer text-xs">Start Survey</button>
                     </div>
                 </div>
             ))}
