0b243b53034c9dd217dd5e8d0895243184709f74
alfathir rizky harsya <alfathirrizky083@gmail.com>
2026-01-17 20:52:10 +0700

    fix: adjustment homepage layout for mobile

--- frontend/src/pages/homePage.jsx
+++ frontend/src/pages/homePage.jsx
@@ -14,36 +14,38 @@ function HomePage() {
 
     return (
         <div className="scroll-smooth overflow-y-hidden">
-            <section className="h-screen flex flex-col items-center justify-center bg-[url('./assets/hero.jpg')] bg-cover bg-center text-white">
-                <h1 className="text-7xl font-bold mb-4">Welcome to the Home Page</h1>
-                <p className="text-xl">This is the home page of the application.</p>
+            <section className="hero h-screen flex flex-col items-center justify-center bg-[url('./assets/hero.jpg')] bg-cover bg-center text-white">
+                <h1 className="text-3xl lg:text-7xl font-bold lg:mb-4">Welcome to the Home Page</h1>
+                <p className="text-lg lg:text-xl">This is the home page of the application.</p>
                 <button className="mt-4 bg-blue-500 text-white px-4 py-2 rounded-2xl transition duration-300 hover:bg-blue-600" onClick={() => navigate("/survei")}>Get Started</button>
             </section>
-            <div className="h-screen flex flex-col items-center justify-center text-white gap-5 px-10">
-                <div className=" mb-5">
-                    <h1 className=" text-black font-bold text-2xl">Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta, sit.</h1>
+            <section className="flex flex-col items-center text-white gap-5 px-3 mt-10 w-full">
+                <h1 className="text-black font-bold text-xl md:text-2xl text-center">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta, sit.
+                </h1>
+                <div className="flex justify-start w-full overflow-x-auto scrollbar-hide snap-x pb-5 md:justify-center">
+                    <div className="flex gap-5">
+                        <div className="snap-center min-w-[320px] h-96 bg-[url('./assets/image2.jpg')] bg-cover bg-center flex flex-col justify-end rounded-2xl p-3">
+                            <div className="bg-white/20 rounded-xl p-5 backdrop-blur-sm h-32 transition-all duration-700 hover:h-full">
+                                <h1 className="font-bold text-2xl">Text 1</h1>
+                                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+                            </div>
+                        </div>
+                        <div className="snap-center min-w-[320px] h-96 bg-[url('./assets/image2.jpg')] bg-cover bg-center flex flex-col justify-end rounded-2xl p-3">
+                            <div className="bg-white/20 rounded-xl p-5 backdrop-blur-sm h-32 transition-all duration-700 hover:h-full">
+                                <h1 className="font-bold text-2xl">Text 2</h1>
+                                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+                            </div>
+                        </div>
+                        <div className="snap-center min-w-[320px] h-96 bg-[url('./assets/image2.jpg')] bg-cover bg-center flex flex-col justify-end rounded-2xl p-3">
+                            <div className="bg-white/20 rounded-xl p-5 backdrop-blur-sm h-32 transition-all duration-700 hover:h-full">
+                                <h1 className="font-bold text-2xl">Text 3</h1>
+                                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+                            </div>
+                        </div>
+                    </div>
                 </div>
-                <div className="flex gap-5">
-                    <div className=" bg-[url('./assets/image2.jpg')] flex flex-col justify-end rounded-2xl h-96 w-sm p-3 shadow-2xl" data-aos="fade-up" data-aos-anchor-placement="center-center" data-aos-delay="100" data-aos-duration="700">
-                        <div className="bg-white/20 rounded-xl p-5 backdrop-blur-sm h-32 transition-all duration-700 delay-300 ease-in-out hover:h-full">
-                            <h1 className=" font-bold text-2xl">Text 1</h1>
-                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis, possimus.</p>
-                        </div>
-                    </div>
-                    <div className=" bg-[url('./assets/image2.jpg')] flex flex-col justify-end rounded-2xl h-96 w-sm p-3 shadow-2xl" data-aos="fade-up" data-aos-anchor-placement="center-center" data-aos-delay="200" data-aos-duration="700">
-                        <div className="bg-white/20 rounded-xl p-5 backdrop-blur-sm h-32 transition-all duration-700 delay-300 ease-in-out hover:h-full">
-                            <h1 className=" font-bold text-2xl">Text 1</h1>
-                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis, possimus.</p>
-                        </div>
-                    </div>
-                    <div className=" bg-[url('./assets/image2.jpg')] flex flex-col justify-end rounded-2xl h-96 w-sm p-3 shadow-2xl" data-aos="fade-up" data-aos-anchor-placement="center-center" data-aos-delay="300" data-aos-duration="700">
-                        <div className="bg-white/20 rounded-xl p-5 backdrop-blur-sm h-32 transition-all duration-700 delay-300 ease-in-out hover:h-full">
-                            <h1 className=" font-bold text-2xl group-hover:hidden">Text 1</h1>
-                            <p className=" line-clamp-3 group-hover:block">Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis, possimus. Lorem ipsum, dolor sit amet consectetur adipisicing elit. Facilis repellendus maiores perferendis aut quos quas cumque consequatur modi consectetur, quaerat laboriosam. Ipsam repudiandae natus ad recusandae fugiat placeat ipsum doloremque blanditiis voluptatum enim, expedita nam, itaque minima delectus nihil a cum, similique sequi! Suscipit, consequatur inventore veritatis voluptatibus maiores optio? </p>
-                        </div>
-                    </div>
-                </div>
-            </div>
+            </section>
             <div className=" h-screen flex justify-between p-10">
                 <h1>hai</h1>
             </div>  
