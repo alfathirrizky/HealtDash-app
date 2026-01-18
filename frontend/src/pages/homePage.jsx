f65ccd3df68729d496934c1d6d7fed6de78bb7f4
alfathir rizky harsya <alfathirrizky083@gmail.com>
2026-01-18 23:01:05 +0700

    add: content on landing page

--- frontend/src/pages/homePage.jsx
+++ frontend/src/pages/homePage.jsx
@@ -1,8 +1,8 @@
 import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import AOS from "aos";
-import Img3 from "../assets/image3.jpg";
 import "aos/dist/aos.css";
+import CardSwap, { Card } from "../components/CardSwap"
 
 function HomePage() {
     const navigate = useNavigate();
@@ -21,18 +21,34 @@ function HomePage() {
             </section>
             <section className="flex flex-col md:flex-row items-center justify-center">
                 <div className="container mx-auto px-4 py-8 flex flex-col items-center justify-center">
-                    <h1 className="text-xl md:text-3xl font-bold mb-2">About This Mental Health Facility</h1>
+                    <h1 className="text-xl md:text-3xl lg:text-4xl font-bold mb-2">About This Mental Health Facility</h1>
                     <p className="text-center text-xs">A company-provided facility to support employee mental wellbeing and reduce work-related stress.</p>
                 </div>
             </section>
-            <section className="flex flex-col md:flex-row items-center justify-center">
-                <div className="container mx-auto px-4 py-8 flex flex-col items-center justify-center">
-                    <h1 className="text-xl md:text-3xl font-bold mb-4 text-center">Why Mental Health Matters at Work?</h1>
+            <section className="flex items-center justify-between px-78">
+                <div className="w-2xl">
+                    <h1 className="text-xl md:text-3xl font-bold mb-4">Why Mental Health Matters at Work?</h1>
+                    <p>This platform is an official internal facility provided by the company to support the mental wellbeing of employees.It is not an external service and not a commercial offering.</p>
+                </div>
+                <div style={{ height: '500px', position: 'relative' }}>
+                    <CardSwap
+                        cardDistance={60}
+                        verticalDistance={70}
+                        delay={4000}
+                        pauseOnHover={true}
+                    >
+                        <Card className="bg-[url('./assets/hero.jpg')] bg-cover bg-center">
+                        </Card>
+                        <Card className="bg-[url('./assets/hero.jpg')] bg-cover bg-center">
+                        </Card>
+                        <Card className="bg-[url('./assets/hero.jpg')] bg-cover bg-center">
+                        </Card>
+                    </CardSwap>
                 </div>
             </section>
-            <section className="features flex flex-col md:flex-row items-center justify-center">
+            <section className="features flex flex-col md:flex-row items-center justify-center mt-55">
                 <div className="container flex flex-col justify-center items-center mx-auto px-4 py-8">
-                    <h1 className="text-xl md:text-3xl font-bold mb-2">Features</h1>
+                    <h1 className="text-xl md:text-3xl lg:text-5xl font-bold mb-2">Features</h1>
                     <p className="text-center text-xs">Our mental health facility offers a range of features to support employee wellbeing:</p>
                 </div>
             </section>
