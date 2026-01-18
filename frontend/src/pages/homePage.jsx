5d341c38f3e351b696dcdf6709a118a6de3e19e7
alfathir rizky harsya <alfathirrizky083@gmail.com>
2026-01-18 18:01:33 +0700

    fix: adjustment landing page layout

--- frontend/src/pages/homePage.jsx
+++ frontend/src/pages/homePage.jsx
@@ -15,40 +15,27 @@ function HomePage() {
     return (
         <div className="scroll-smooth overflow-y-hidden">
             <section className="hero h-screen flex flex-col items-center justify-center bg-[url('./assets/hero.jpg')] bg-cover bg-center text-white">
-                <h1 className="text-3xl md:text-6xl font-bold lg:mb-4">Welcome to the Home Page</h1>
-                <p className="text-lg lg:text-3xl">This is the home page of the application.</p>
+                <h1 className="text-2xl mb-3 text-center md:text-4xl font-bold lg:mb-4">A Company Facility to Support Employee Mental Wellbeing</h1>
+                <p className="text-sm text-center md:text-lg lg:text-2xl">An internal platform provided by the company to help employees manage work-related stress, maintain mental wellbeing, and stay productive.</p>
                 <button className="mt-4 bg-blue-500 text-white px-4 py-2 rounded-2xl transition duration-300 hover:bg-blue-600 cursor-pointer" onClick={() => navigate("/survei")}>Get Started</button>
             </section>
-            <section className="flex flex-col items-center text-white gap-10 px-5 mt-10 w-full">
-                <h1 className="text-black font-bold text-xl md:text-3xl text-center">
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta, sit.
-                </h1>
-                <div className="flex justify-start w-full overflow-x-auto scrollbar-hide snap-x pb-5 lg:justify-center touchpan">
-                    <div className="flex gap-5">
-                        <div className="snap-center min-w-[320px] h-96 bg-[url('./assets/image2.jpg')] bg-cover bg-center flex flex-col justify-end rounded-2xl p-3">
-                            <div className="bg-white/20 rounded-xl p-5 backdrop-blur-sm h-32 transition-all duration-700 hover:h-full">
-                                <h1 className="font-bold text-2xl">Text 1</h1>
-                                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
-                            </div>
-                        </div>
-                        <div className="snap-center min-w-[320px] h-96 bg-[url('./assets/image2.jpg')] bg-cover bg-center flex flex-col justify-end rounded-2xl p-3">
-                            <div className="bg-white/20 rounded-xl p-5 backdrop-blur-sm h-32 transition-all duration-700 hover:h-full">
-                                <h1 className="font-bold text-2xl">Text 2</h1>
-                                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
-                            </div>
-                        </div>
-                        <div className="snap-center min-w-[320px] h-96 bg-[url('./assets/image2.jpg')] bg-cover bg-center flex flex-col justify-end rounded-2xl p-3">
-                            <div className="bg-white/20 rounded-xl p-5 backdrop-blur-sm h-32 transition-all duration-700 hover:h-full">
-                                <h1 className="font-bold text-2xl">Text 3</h1>
-                                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
-                            </div>
-                        </div>
-                    </div>
+            <section className="flex flex-col md:flex-row items-center justify-center">
+                <div className="container mx-auto px-4 py-8 flex flex-col items-center justify-center">
+                    <h1 className="text-xl md:text-3xl font-bold mb-2">About This Mental Health Facility</h1>
+                    <p className="text-center text-xs">A company-provided facility to support employee mental wellbeing and reduce work-related stress.</p>
                 </div>
             </section>
-            <div className=" h-screen flex justify-between p-10">
-                <h1>hai</h1>
-            </div>  
+            <section className="flex flex-col md:flex-row items-center justify-center">
+                <div className="container mx-auto px-4 py-8 flex flex-col items-center justify-center">
+                    <h1 className="text-xl md:text-3xl font-bold mb-4 text-center">Why Mental Health Matters at Work?</h1>
+                </div>
+            </section>
+            <section className="features flex flex-col md:flex-row items-center justify-center">
+                <div className="container flex flex-col justify-center items-center mx-auto px-4 py-8">
+                    <h1 className="text-xl md:text-3xl font-bold mb-2">Features</h1>
+                    <p className="text-center text-xs">Our mental health facility offers a range of features to support employee wellbeing:</p>
+                </div>
+            </section>
         </div>
     )
 }
