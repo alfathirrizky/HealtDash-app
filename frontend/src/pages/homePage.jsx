7cc61d9f97ebdac25f562d04efe3bbff7de7603e
alfathir rizky harsya <alfathirrizky083@gmail.com>
2026-01-20 16:30:27 +0700

    add: add component stepper on homepage

--- frontend/src/pages/homePage.jsx
+++ frontend/src/pages/homePage.jsx
@@ -4,6 +4,7 @@ import AOS from "aos";
 import "aos/dist/aos.css";
 import CardSwap, { Card } from "../components/CardSwap"
 import SpotlightCard from '../components/SpotlightCard';
+import Stepper, { Step } from '../components/Stepper';
 
 function HomePage() {
     const navigate = useNavigate();
@@ -82,10 +83,39 @@ function HomePage() {
                     </SpotlightCard>
                 </div>
             </section>
-            <section className="features flex flex-col md:flex-row items-center justify-center mt-55">
+            <section className="features flex flex-col items-center justify-center gap-5 h-screen">
                 <div className="container flex flex-col justify-center items-center mx-auto px-4 py-8">
                     <h1 className="text-xl md:text-3xl lg:text-5xl font-bold mb-2">Features</h1>
                     <p className="text-center text-xs">Our mental health facility offers a range of features to support employee wellbeing:</p>
+                </div>
+                <div className="w-xl">
+                    <Stepper
+                    initialStep={1}
+                    onStepChange={(step) => {
+                        console.log(step);
+                    }}
+                    onFinalStepCompleted={() => console.log("All steps completed!")}
+                    backButtonText="Previous"
+                    nextButtonText="Next"
+                    >
+                        <Step>
+                            <h2>Welcome to the React Bits stepper!</h2>
+                            <p>Check out the next step!</p>
+                        </Step>
+                        <Step>
+                            <h2>Step 2</h2>
+                            <img style={{ height: '100px', width: '100%', objectFit: 'cover', objectPosition: 'center -70px', borderRadius: '15px', marginTop: '1em' }} src="https://www.purrfectcatgifts.co.uk/cdn/shop/collections/Funny_Cat_Cards_640x640.png?v=1663150894" />
+                            <p>Custom step content!</p>
+                        </Step>
+                        <Step>
+                            <h2>How about an input?</h2>
+                            {/* <input value={name} onChange={(e) => setName(e.target.value)} placeholder="Your name?" /> */}
+                        </Step>
+                        <Step>
+                            <h2>Final Step</h2>
+                            <p>You made it!</p>
+                        </Step>
+                    </Stepper>
                 </div>
             </section>
         </div>
